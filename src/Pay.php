01bb642fc7358dc6fc39bfe0b7cdc6f96b726e47
xin <chan_jam@outlook.com>
2017-03-28 18:17:02 +0800

    增加预支付请求，用于获取预支付信息，生成支付二维码

--- src/Pay.php
+++ src/Pay.php
@@ -11,14 +11,14 @@ class Pay
 
     /**
      * 创建App支付
-     * @param string                   $appId               支付宝应用AppId
-     * @param string                   $rsaPrivateKey       私钥字符
-     * @param string                   $alipayRsaPublicKey  支付宝公钥字符
-     * @param AlipayTradeAppPayRequest $appPayRequest       App支付请求对象
-     * @param bool|false               $sandBox             沙盒开启
+     * @param string                   $appId              支付宝应用AppId
+     * @param string                   $rsaPrivateKey      私钥字符
+     * @param string                   $alipayRsaPublicKey 支付宝公钥字符
+     * @param AlipayTradeAppPayRequest $request            App支付请求对象
+     * @param bool|false               $sandBox            沙盒开启
      * @return string
      */
-    public static function createAppPay($appId, $rsaPrivateKey, $alipayRsaPublicKey, $appPayRequest, $sandBox = false) {
+    public static function createAppPay($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox = false) {
         $aop = new AopClient;
         // 根据是否沙盒模式决定网关地址
         $gatewayUrl = $sandBox ? 'https://openapi.alipaydev.com/gateway.do' : 'https://openapi.alipay.com/gateway.do';
@@ -29,22 +29,23 @@ class Pay
         $aop->postCharset = 'UTF-8';
         $aop->signType = 'RSA2';
         $aop->alipayrsaPublicKey = $alipayRsaPublicKey;
+        $aop->debugInfo = true;
 
-        $response = $aop->sdkExecute($appPayRequest);
+        $response = $aop->sdkExecute($request);
 
         return htmlspecialchars($response);
     }
 
     /**
-     * @param string                   $appId               支付宝应用AppId
-     * @param string                   $rsaPrivateKey       私钥字符
-     * @param string                   $alipayRsaPublicKey  支付宝公钥字符
-     * @param AlipayTradeWapPayRequest $wapPayRequest       Wap支付请求对象
-     * @param bool|false               $sandBox             沙盒开启
+     * @param string                   $appId              支付宝应用AppId
+     * @param string                   $rsaPrivateKey      私钥字符
+     * @param string                   $alipayRsaPublicKey 支付宝公钥字符
+     * @param AlipayTradeWapPayRequest $request            Wap支付请求对象
+     * @param bool|false               $sandBox            沙盒开启
      * @return string|提交表单HTML文本
      * @throws Exception
      */
-    public static function createFormPay($appId, $rsaPrivateKey, $alipayRsaPublicKey, $wapPayRequest, $sandBox = false) {
+    public static function createWapPay($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox = false) {
         $aop = new AopClient;
         // 根据是否沙盒模式决定网关地址
         $gatewayUrl = $sandBox ? 'https://openapi.alipaydev.com/gateway.do' : 'https://openapi.alipay.com/gateway.do';
@@ -55,9 +56,34 @@ class Pay
         $aop->postCharset = 'UTF-8';
         $aop->signType = 'RSA2';
         $aop->alipayrsaPublicKey = $alipayRsaPublicKey;
+        $aop->debugInfo = true;
 
-        $response = $aop->pageExecute($wapPayRequest);
+        return $aop->pageExecute($request);
+    }
 
-        return $response;
+    /**
+     * 生成预支付信息
+     * @param string                      $appId              支付宝应用AppId
+     * @param string                      $rsaPrivateKey      私钥字符
+     * @param string                      $alipayRsaPublicKey 支付宝公钥字符
+     * @param AlipayTradePrecreateRequest $request            预支付请求对象
+     * @param bool|false                  $sandBox            沙盒开启
+     * @return bool|mixed|\SimpleXMLElement
+     * @throws Exception
+     */
+    public static function createPrePay($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox = false) {
+        $aop = new AopClient;
+        // 根据是否沙盒模式决定网关地址
+        $gatewayUrl = $sandBox ? 'https://openapi.alipaydev.com/gateway.do' : 'https://openapi.alipay.com/gateway.do';
+        $aop->gatewayUrl = $gatewayUrl;
+        $aop->appId = $appId;
+        $aop->rsaPrivateKey = $rsaPrivateKey;
+        $aop->format = 'json';
+        $aop->postCharset = 'UTF-8';
+        $aop->signType = 'RSA2';
+        $aop->alipayrsaPublicKey = $alipayRsaPublicKey;
+        $aop->debugInfo = true;
+
+        return $aop->execute($request);
     }
 }
