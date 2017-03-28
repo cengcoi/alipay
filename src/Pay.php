c81398e1165e8900c86b099b7b64ec17c4822f2a
xin <chan_jam@outlook.com>
2017-03-28 18:30:03 +0800

    修改提交主类

--- src/Pay.php
+++ src/Pay.php
@@ -8,82 +8,78 @@ namespace TodChan\Alipay;
  */
 class Pay
 {
+    private static $aop;
 
+
+    public static function makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox = false){
+        if(!self::$aop){
+            self::$aop = new AopClient;
+            // 根据是否沙盒模式决定网关地址
+            $gatewayUrl = $sandBox ? 'https://openapi.alipaydev.com/gateway.do' : 'https://openapi.alipay.com/gateway.do';
+            self::$aop->gatewayUrl = $gatewayUrl;
+            self::$aop->appId = $appId;
+            self::$aop->rsaPrivateKey = $rsaPrivateKey;
+            self::$aop->format = 'json';
+            self::$aop->postCharset = 'UTF-8';
+            self::$aop->signType = 'RSA2';
+            self::$aop->alipayrsaPublicKey = $alipayRsaPublicKey;
+            self::$aop->debugInfo = true;
+        }
+
+        return self::$aop;
+    }
     /**
      * 创建App支付
+     * @param AlipayTradeAppPayRequest $request            App支付请求对象
      * @param string                   $appId              支付宝应用AppId
      * @param string                   $rsaPrivateKey      私钥字符
      * @param string                   $alipayRsaPublicKey 支付宝公钥字符
-     * @param AlipayTradeAppPayRequest $request            App支付请求对象
      * @param bool|false               $sandBox            沙盒开启
      * @return string
      */
-    public static function createAppPay($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox = false) {
-        $aop = new AopClient;
-        // 根据是否沙盒模式决定网关地址
-        $gatewayUrl = $sandBox ? 'https://openapi.alipaydev.com/gateway.do' : 'https://openapi.alipay.com/gateway.do';
-        $aop->gatewayUrl = $gatewayUrl;
-        $aop->appId = $appId;
-        $aop->rsaPrivateKey = $rsaPrivateKey;
-        $aop->format = 'json';
-        $aop->postCharset = 'UTF-8';
-        $aop->signType = 'RSA2';
-        $aop->alipayrsaPublicKey = $alipayRsaPublicKey;
-        $aop->debugInfo = true;
-
+    public static function createAppPay($request, $appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox = false) {
+        $aop = self::makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox);
         $response = $aop->sdkExecute($request);
 
         return htmlspecialchars($response);
     }
 
     /**
+     * @param AlipayTradeWapPayRequest $request            Wap支付请求对象
      * @param string                   $appId              支付宝应用AppId
      * @param string                   $rsaPrivateKey      私钥字符
      * @param string                   $alipayRsaPublicKey 支付宝公钥字符
-     * @param AlipayTradeWapPayRequest $request            Wap支付请求对象
      * @param bool|false               $sandBox            沙盒开启
      * @return string|提交表单HTML文本
      * @throws Exception
      */
     public static function createWapPay($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox = false) {
-        $aop = new AopClient;
-        // 根据是否沙盒模式决定网关地址
-        $gatewayUrl = $sandBox ? 'https://openapi.alipaydev.com/gateway.do' : 'https://openapi.alipay.com/gateway.do';
-        $aop->gatewayUrl = $gatewayUrl;
-        $aop->appId = $appId;
-        $aop->rsaPrivateKey = $rsaPrivateKey;
-        $aop->format = 'json';
-        $aop->postCharset = 'UTF-8';
-        $aop->signType = 'RSA2';
-        $aop->alipayrsaPublicKey = $alipayRsaPublicKey;
-        $aop->debugInfo = true;
+        $aop = self::makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox);
 
         return $aop->pageExecute($request);
     }
 
     /**
      * 生成预支付信息
+     * @param AlipayTradePrecreateRequest $request            预支付请求对象
      * @param string                      $appId              支付宝应用AppId
      * @param string                      $rsaPrivateKey      私钥字符
      * @param string                      $alipayRsaPublicKey 支付宝公钥字符
-     * @param AlipayTradePrecreateRequest $request            预支付请求对象
      * @param bool|false                  $sandBox            沙盒开启
      * @return bool|mixed|\SimpleXMLElement
      * @throws Exception
      */
-    public static function createPrePay($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox = false) {
-        $aop = new AopClient;
-        // 根据是否沙盒模式决定网关地址
-        $gatewayUrl = $sandBox ? 'https://openapi.alipaydev.com/gateway.do' : 'https://openapi.alipay.com/gateway.do';
-        $aop->gatewayUrl = $gatewayUrl;
-        $aop->appId = $appId;
-        $aop->rsaPrivateKey = $rsaPrivateKey;
-        $aop->format = 'json';
-        $aop->postCharset = 'UTF-8';
-        $aop->signType = 'RSA2';
-        $aop->alipayrsaPublicKey = $alipayRsaPublicKey;
-        $aop->debugInfo = true;
+    public static function createPrePay($request, $appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox = false) {
+        $aop = self::makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox);
 
         return $aop->execute($request);
     }
+
+    public static function query($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox = false){
+        $aop = self::makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox);
+        
+        return $aop->execute($request);
+    }
+
+
 }
