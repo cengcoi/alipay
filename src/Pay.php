68bd46d7cb2090f938e55c793a2d3445045d577d
xin <chan_jam@outlook.com>
2017-03-29 09:47:05 +0800

    修复

--- src/Pay.php
+++ src/Pay.php
@@ -8,11 +8,11 @@ namespace TodChan\Alipay;
  */
 class Pay
 {
+
     private static $aop;
 
-
-    public static function makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox = false){
-        if(!self::$aop){
+    public static function makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox = false) {
+        if (!self::$aop) {
             self::$aop = new AopClient;
             // 根据是否沙盒模式决定网关地址
             $gatewayUrl = $sandBox ? 'https://openapi.alipaydev.com/gateway.do' : 'https://openapi.alipay.com/gateway.do';
@@ -28,6 +28,7 @@ class Pay
 
         return self::$aop;
     }
+
     /**
      * 创建App支付
      * @param AlipayTradeAppPayRequest $request            App支付请求对象
@@ -38,7 +39,7 @@ class Pay
      * @return string
      */
     public static function createAppPay($request, $appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox = false) {
-        $aop = self::makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox);
+        $aop = self::makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox);
         $response = $aop->sdkExecute($request);
 
         return htmlspecialchars($response);
@@ -53,8 +54,8 @@ class Pay
      * @return string|提交表单HTML文本
      * @throws Exception
      */
-    public static function createWapPay($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox = false) {
-        $aop = self::makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox);
+    public static function createWapPay($request, $appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox = false) {
+        $aop = self::makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox);
 
         return $aop->pageExecute($request);
     }
@@ -70,16 +71,25 @@ class Pay
      * @throws Exception
      */
     public static function createPrePay($request, $appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox = false) {
-        $aop = self::makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox);
+        $aop = self::makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox);
 
         return $aop->execute($request);
     }
 
-    public static function query($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox = false){
-        $aop = self::makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $request, $sandBox);
+    /**
+     * 查询订单
+     * @param AlipayTradeQueryRequest $request            查询请求对象
+     * @param string                  $appId              支付宝应用AppId
+     * @param string                  $rsaPrivateKey      私钥字符
+     * @param string                  $alipayRsaPublicKey 支付宝公钥字符
+     * @param bool|false              $sandBox            沙盒开启
+     * @return bool|mixed|\SimpleXMLElement
+     * @throws Exception
+     */
+    public static function query($request, $appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox = false) {
+        $aop = self::makeAop($appId, $rsaPrivateKey, $alipayRsaPublicKey, $sandBox);
 
         return $aop->execute($request);
     }
-
 
 }
